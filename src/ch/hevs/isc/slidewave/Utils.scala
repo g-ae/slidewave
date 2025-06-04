bfb92c9f15157eabb5c23e55d6d48d1fb66de743
gonçalo <goncalo.arieira@hotmail.com>
2025-06-04 22:12:41 +0200

    Better lap time text

--- src/ch/hevs/isc/slidewave/Utils.scala
+++ src/ch/hevs/isc/slidewave/Utils.scala
@@ -3,8 +3,15 @@ package ch.hevs.isc.slidewave
 object Utils {
   /**
    * returns time in seconds, string will not contain "s" for seconds
-   * @param ms
-   * @return
    */
-  def msToSecondsStr(ms: Double): String = (ms / 1000).toString
+  def msToSecondsStr(ms: Double): String = {
+    val final_ms = (ms % 1000).toInt
+    val seconds = Math.floor(ms / 1000)
+    val final_s = (seconds % 60).toInt
+    val minutes = Math.floor(seconds / 60)
+    val final_min = (minutes % 60).toInt
+
+
+    (if (final_min != 0) f"$final_min%02d:" else "") + f"$final_s%02d.$final_ms%03d"
+  }
 }
